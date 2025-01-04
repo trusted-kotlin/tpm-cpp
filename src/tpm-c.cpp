4095f943d349481b2e1b9e8cb7e00e93f8b65604
cach30verfl0w <cach30verfl0w@gmail.com>
2025-01-04 19:55:29 +0100

    Fix C API

--- src/tpm-c.cpp
+++ src/tpm-c.cpp
@@ -33,20 +33,20 @@ tpm_error_t tpm_context_init(tpm_object_t* context) {
     }
 
     try {
-        *reinterpret_cast<tpm::TPMContext*>(context->context.context) = tpm::TPMContext();
+        context->context.context = new tpm::TPMContext();
         return TPM_ERROR_SUCCESS;
-    } catch (std::runtime_error& ex) {
+    } catch (std::runtime_error& ignored) {
         return TPM_ERROR_OPERATION_FAILED;
     }
 }
 
-tpm_error_t tpm_context_message(const tpm_object_t* context, uint8_t* msg, uint32_t msg_length, tpm_object* response) {
+tpm_error_t tpm_context_message(const tpm_object_t* context, const uint8_t* msg, const uint32_t msg_length, const tpm_object* response) {
     if (context->type != TPM_OBJECT_CONTEXT && context->type != TPM_OBJECT_MESSAGE) {
         return TPM_ERROR_INVALID_OBJECT;
     }
 
-    const auto* tpm_context = reinterpret_cast<tpm::TPMContext*>(context->context.context);
-    auto message = std::vector<uint8_t>(msg, msg + msg_length);
+    const auto* tpm_context = static_cast<tpm::TPMContext*>(context->context.context);
+    const auto message = std::vector(msg, msg + msg_length);
 
     try {
         auto* response_vector = tpm_context->emit_message(message);
@@ -54,7 +54,7 @@ tpm_error_t tpm_context_message(const tpm_object_t* context, uint8_t* msg, uint3
         response_object.length = response_vector->size();
         response_object.message = static_cast<void*>(response_vector);
 
-    } catch (std::runtime_error& ex) {
+    } catch (std::runtime_error& ignored) {
         return TPM_ERROR_OPERATION_FAILED;
     }
 
@@ -70,10 +70,12 @@ TPMCPP_API void* tpm_message_get_data(const tpm_object_t* object) {
 }
 
 tpm_error_t tpm_context_close(const tpm_object_t* object) {
-    if (object->type != TPM_OBJECT_MESSAGE) {
-        delete reinterpret_cast<std::vector<uint8_t>*>(object->message.message);
-    } else if (object->type != TPM_OBJECT_CONTEXT) {
-        reinterpret_cast<tpm::TPMContext*>(object->context.context)->~TPMContext();
+    if (object->type == TPM_OBJECT_MESSAGE) {
+        delete static_cast<std::vector<uint8_t>*>(object->message.message);
+    } else if (object->type == TPM_OBJECT_CONTEXT) {
+        const auto context = static_cast<tpm::TPMContext*>(object->context.context);
+        context->~TPMContext();
+        delete context;
     }
     return TPM_ERROR_INVALID_OBJECT;
 }
